128404b52edd1468fe2efd6286603b0c6eb64bd1
IRGO-CO <125756540+IRGO-CO@users.noreply.github.com>
2023-04-26 01:36:05 -0300

    Commit atrasado de todas as alterações
    Adicionado parte do conteudo do site, iniciado a estilização da responsividade, e alterações nas cores, nome, etc

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$('.slide-principal').slick({
+  centerMode: false,
+  centerPadding: '50px',
+  slidesToShow: 1,
+  autoplay: true,
+  focusOnSelect: true,
+  speed: 3000,
+  fade: true,
+  arrows: false,
+  autoplaySpeed: 5000,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+  swipe: false
+});
